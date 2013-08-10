a85ba3f6bd946185254b203c02df9b411ed4f31a
Ziqi Wu <Ziqi.Wu@bmw.com>
2013-08-10 21:06:38 +0800

    Ziqi:添加资源，我觉得这个方式不如重新新项目

---
+++ code/wetongji2_ios_mondev/WeTongji/WeHomeViewController.h
@@ -0,0 +1,14 @@
+//
+//  WeHomeViewController.h
+//  WeTongji
+//
+//  Created by 吴 wuziqi on 13-8-10.
+//  Copyright (c) 2013年 Tongji Apple Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WTRootViewController.h"
+
+@interface WeHomeViewController : WTRootViewController <UIScrollViewDelegate>
+
+@end
