db6ebe6ce0a29d2ac7121e1d3da4c3a9266e2b3e
Ziqi Wu <Ziqi.Wu@bmw.com>
2013-08-15 17:04:43 +0800

    Ziqi:添加自定义content

---
+++ WeCampus/WEActivityDetailContentView.h
@@ -0,0 +1,13 @@
+//
+//  WEActivityDetailContentView.h
+//  WeCampus
+//
+//  Created by 吴 wuziqi on 13-8-15.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WEActivityDetailContentView : UIView
+
+@end
---
+++ WeCampus/WEActivityDetailContentView.m
@@ -0,0 +1,31 @@
+//
+//  WEActivityDetailContentView.m
+//  WeCampus
+//
+//  Created by 吴 wuziqi on 13-8-15.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import "WEActivityDetailContentView.h"
+
+@implementation WEActivityDetailContentView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
