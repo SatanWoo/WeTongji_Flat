63b254f707d766f16e0fbdc89d626d7e71ffb95d
Ziqi Wu <Ziqi.Wu@bmw.com>
2013-08-15 13:23:50 +0800

    Ziqi:开始制作活动详情

---
+++ WeCampus/WEActivityDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  WEActivityDetailViewController.h
+//  WeCampus
+//
+//  Created by 吴 wuziqi on 13-8-15.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import "WEContentViewController.h"
+
+@interface WEActivityDetailViewController : WEContentViewController
+
+@end
---
+++ WeCampus/WEActivityDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  WEActivityDetailViewController.m
+//  WeCampus
+//
+//  Created by 吴 wuziqi on 13-8-15.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import "WEActivityDetailViewController.h"
+
+@interface WEActivityDetailViewController ()
+
+@end
+
+@implementation WEActivityDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
