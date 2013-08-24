0ccc303194bab72930fb9e384dd0134d5938aa99
Song <sbhhbscbfv@gmail.com>
2013-08-24 17:42:10 +0800

    person detail

---
+++ WeCampus/WEMeDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  WEMeDetailViewController.h
+//  WeCampus
+//
+//  Created by Song on 13-8-24.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WEMeDetailViewController : UIViewController
+
+@end
---
+++ WeCampus/WEMeDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  WEMeDetailViewController.m
+//  WeCampus
+//
+//  Created by Song on 13-8-24.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import "WEMeDetailViewController.h"
+
+@interface WEMeDetailViewController ()
+
+@end
+
+@implementation WEMeDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
