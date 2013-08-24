ef1e6655ac4be85b14dc73d7c1d2e458ad8b7be0
Song <sbhhbscbfv@gmail.com>
2013-08-25 01:13:47 +0800

    保存为联系人

--- WeCampus/WEMeDetailViewController.m
+++ WeCampus/WEMeDetailViewController.m
@@ -11,6 +11,9 @@
 #import "UIImageView+AsyncLoading.h"
 #import <QuartzCore/QuartzCore.h>
 #import "WTCoreDataManager.h"
+#import <RHAddressBook/RHAddressBook.h>
+#import <RHPerson.h>
+#import <AddressBook/AddressBook.h>
 
 @interface WEMeDetailViewController ()
 {
@@ -61,7 +64,63 @@
 
 - (IBAction)saveAsContact:(id)sender
 {
+    RHAddressBook *ab = [[RHAddressBook alloc] init];
+    if ([RHAddressBook authorizationStatus] == RHAuthorizationStatusNotDetermined){
+        
+        //request authorization
+        [ab requestAuthorizationWithCompletion:^(bool granted, NSError *error) {
+            //[abViewController setAddressBook:ab];
+        }];
+    }
     
+    RHPerson *person = [ab newPersonInDefaultSource];
+    if(_user.name)
+        [person setFirstName:_user.name];
+    if(self.headImageView.image)
+        [person setImage:self.headImageView.image];
+    if(_user.department)
+        [person setDepartment:_user.department];
+    if(_user.birthday)
+        [person setBirthday:_user.birthday];
+    
+    if(_user.phoneNumber && ![_user.phoneNumber isEqualToString:@""])
+    {
+        RHMultiStringValue *phoneMultiValue = [person phoneNumbers];
+        RHMutableMultiStringValue *mutablePhoneMultiValue = [phoneMultiValue mutableCopy];
+        if (! mutablePhoneMultiValue) mutablePhoneMultiValue = [[RHMutableMultiStringValue alloc] initWithType:kABMultiStringPropertyType];
+        
+        //RHPersonPhoneIPhoneLabel casts kABPersonPhoneIPhoneLabel to the correct toll free bridged type, see RHPersonLabels.h
+        [mutablePhoneMultiValue addValue:_user.phoneNumber withLabel:RHPersonPhoneMobileLabel];
+        person.phoneNumbers = mutablePhoneMultiValue;
+    }
+    if(_user.qqAccount && ![_user.qqAccount isEqualToString:@""])
+    {
+        RHMutableMultiStringValue *qqMultiValue = [[RHMutableMultiStringValue alloc] initWithType:kABMultiStringPropertyType];
+        
+        [qqMultiValue addValue:_user.qqAccount withLabel:RHPersonInstantMessageServiceQQ];
+        [person setInstantMessageServices:qqMultiValue];
+    }
+    if(_user.emailAddress && ![_user.emailAddress isEqualToString:@""])
+    {
+        RHMutableMultiStringValue *emailMultiValue = [[RHMutableMultiStringValue alloc] initWithType:kABMultiStringPropertyType];
+        
+        [emailMultiValue addValue:_user.emailAddress withLabel:RHWorkLabel];
+        [person setEmails:emailMultiValue];
+    }
+      
+    [ab addPerson:person];
+    NSError* error = nil;
+    [ab saveWithError:&error];
+    if(!error)
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"成功", nil) message:NSLocalizedString(@"联系人保存成功", nil)  delegate:nil cancelButtonTitle:NSLocalizedString(@"好的", nil)  otherButtonTitles: nil];
+        [alert show];
+    }
+    else
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"失败", nil) message:NSLocalizedString(@"联系人保存失败", nil)  delegate:nil cancelButtonTitle:NSLocalizedString(@"好的", nil)  otherButtonTitles: nil];
+        [alert show];
+    }
 }
 
 - (void)configureWithUser:(User*)user
