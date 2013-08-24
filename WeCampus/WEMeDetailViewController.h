4abd70e21aaed4fc95408d8b954e18a036adf393
Song <sbhhbscbfv@gmail.com>
2013-08-24 20:03:18 +0800

    User detail ing...

--- WeCampus/WEMeDetailViewController.h
+++ WeCampus/WEMeDetailViewController.h
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "User+Addition.h"
 
 @interface WEMeDetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *headImageView;
+@property (weak, nonatomic) IBOutlet UILabel *mottoLabel;
+@property (weak, nonatomic) IBOutlet UILabel *birthdayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *schoolLabel;
+@property (weak, nonatomic) IBOutlet UILabel *departmentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *mobileLabel;
+@property (weak, nonatomic) IBOutlet UILabel *emailLabel;
+@property (weak, nonatomic) IBOutlet UILabel *qqLabel;
+
+- (void)configureWithUser:(User*)user;
 
 @end
--- WeCampus/WEMeDetailViewController.m
+++ WeCampus/WEMeDetailViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "WEMeDetailViewController.h"
+#import "NSDate+WTAddition.h"
+#import "UIImageView+AsyncLoading.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface WEMeDetailViewController ()
 
@@ -33,6 +36,22 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)configureWithUser:(User*)user
+{
+    self.mobileLabel.text = user.motto;
+    self.birthdayLabel.text = [user.birthday convertToYearMonthDayString];
+    self.numberLabel.text = user.studentNumber;
+    self.schoolLabel.text = user.major;
+    self.departmentLabel.text = user.department;
+    self.mobileLabel.text = user.phoneNumber;
+    self.emailLabel.text = user.emailAddress;
+    self.qqLabel.text = user.qqAccount;
+    
+    self.headImageView.layer.masksToBounds = YES;
+    self.headImageView.layer.cornerRadius = self.headImageView.bounds.size.width / 2;
+    [self.headImageView loadImageWithImageURLString:user.avatar];
 }
 
 @end
