7ca37eab604716f7974cc23bee9f4e100b1e8597
Ziqi Wu <Ziqi.Wu@bmw.com>
2013-08-27 23:56:56 +0800

    Ziqi:开始制作邀请好友参加界面

---
+++ WeCampus/WEInviteFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  WEInviteFriendsViewController.h
+//  WeCampus
+//
+//  Created by 吴 wuziqi on 13-8-27.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import "WEContentViewController.h"
+
+@interface WEInviteFriendsViewController : WEContentViewController
+
+@end
---
+++ WeCampus/WEInviteFriendsViewController.m
@@ -0,0 +1,38 @@
+//
+//  WEInviteFriendsViewController.m
+//  WeCampus
+//
+//  Created by 吴 wuziqi on 13-8-27.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import "WEInviteFriendsViewController.h"
+
+@interface WEInviteFriendsViewController ()
+
+@end
+
+@implementation WEInviteFriendsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
