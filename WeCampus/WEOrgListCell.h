b85bf246a43affda76aaf32bc3b920b31dbfd43f
Ziqi Wu <Ziqi.Wu@bmw.com>
2013-08-29 13:54:51 +0800

    Ziqi: 开始制作我喜欢的组织机构列表

---
+++ WeCampus/WEOrgListCell.h
@@ -0,0 +1,13 @@
+//
+//  WEOrgListCell.h
+//  WeCampus
+//
+//  Created by 吴 wuziqi on 13-8-29.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WEOrgListCell : UITableViewCell
+
+@end
---
+++ WeCampus/WEOrgListCell.m
@@ -0,0 +1,29 @@
+//
+//  WEOrgListCell.m
+//  WeCampus
+//
+//  Created by 吴 wuziqi on 13-8-29.
+//  Copyright (c) 2013年 Ziqi Wu. All rights reserved.
+//
+
+#import "WEOrgListCell.h"
+
+@implementation WEOrgListCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
